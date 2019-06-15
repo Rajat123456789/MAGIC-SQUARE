94b8379f04093a839011d59381ff9a322df46156
Rajat123456789 <50230030+Rajat123456789@users.noreply.github.com>
2019-06-15 21:38:04 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,59 +1,49 @@
-#function start
-def magic(a):
-    s=(a**3+a)/2 # constant value for a magic square
-    return s
-#function end
+mat = []
 
-#main
-n= int(input("Enter order"))
-y= magic(n)   # constant sum of the square
+def main():
+    print("Enter the odd order of a square matrix: ")
+    n = int(input())
+    for i in range(0, n):
+        mat.append([])
+    for i in range(0, n):
+        for j in range(0, n):   # initializing matrix
+            mat[i].append(j)
+            mat[i][j] = 0
 
-r=[int(x)for x in input("Enter the elements").split()] # to have multiple inputs
+    magic_const = (n * (n ** 2 + 1) // 2)
 
-#to print the elements of the list
-for i in range (0,n**2,n):    # from zero till n squared -1 in intervals of n
-    for j in range (i,i+n,1):  # from i till i+n-1 in intervals of 1
-        print("%d"%r[j])
-    print("\n")
-#printing of elements of list over
 
-# sum of rows
-s=0  #initializing sum as zero
-flag=1
-for i in range (0,n**2,n):
-    for j in range (i,i+n,1):
-                s+=r[j]
-    if(s!=y):
-       flag=0
-    break
-s=0
+    print("Magic Square constant is: ")
+    print(magic_const)
+    odd_magic_square_generator(n)  # a constant(sum) by any row,column or diagonal
 
-#sum of columns
-i=0
-j=0
-s=0
-if (flag==1):
-    for i in range (n):
-        for j in range (i,n**2+i,n):
-            s+=r[j]
-        if(s!=y):
-            flag=0
-        break
-s=0
 
-#diagonals
-if(flag==1):
-    for i in range(0, n**2, n + 1):
-        s = s + r[i]
-    if (s != y):
-        flag = 0
-    s = 0
-    for j in range(n - 1, n**2 - 1, n - 1):
-        s = s + r[j]
-    if (s != y):
-        flag  = 0
-        # final result
-if (flag == 0):
-    print("It is not magic square")
-else:
-    print("It is magic square")
+
+# Odd Magic Squares
+def odd_magic_square_generator(n):
+    i = n // 2
+    j = n - 1
+    mat[i][j] = 1
+
+    for p in range(2, n ** 2 + 1):
+        i = i - 1
+        j = j + 1
+        if (i == -1 & j != n):
+            i = n - 1
+        elif (j == n & i != -1):
+            j = 0
+        elif (i == -1 & j == n):
+            i = 0
+            j = n - 2
+
+        if mat[i][j] != 0:
+            i = i + 1
+            j = j - 2
+        mat[i][j] = p
+
+    print("The magic Square generated is: ")
+    for i in range(n):
+        for j in range(n):
+            print(mat[i][j], end=" ")  # printing
+        print("\n")
+main()
